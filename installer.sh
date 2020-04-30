6b62231ad6751a8166db5be0a603a171d80698df
Peter Mosmans <support@go-forward.net>
2020-04-30 20:37:53 +0200

    feat: update installer

--- installer.sh
+++ installer.sh
@@ -10,10 +10,14 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
+# shellcheck disable=SC2034
+VERSION=1.3
+
 # For configuration, use the INIFILE
 INIFILE=.installerrc
 
 # Define associative arrays
+declare -A aliases
 declare -A copyfiles
 declare -A linkfiles
 declare -A osfiles
@@ -21,7 +25,18 @@ declare -A osfiles
 
 
 # The file .installerrc should contain all installer-specific files:
-# copyfiles, linkfiles, osfiles and executables.
+# aliases, copyfiles, linkfiles, osfiles and executables.
+
+# aliases: Aliases (links) to files. an array of targets and link names. The
+# targets are from the relative locations from where they will be installed.
+#
+# Example:
+# aliases=(
+#     ["lintjinja.py"]="lintjinja"
+# )
+#
+# This will link target lintjinja.py to link name lintjinja, both in the target
+# directory.
 
 # copyfiles: Files to copy.
 # An array of relative location from where the script is run, and files.
@@ -67,28 +82,31 @@ declare -A osfiles
 # This will search for lint-jenkins, and emits a warning if the file cannot be found/executed.
 # A warning will be shown if any of the files cannot be found (on the search paths)
 
-# shellcheck disable=SC2034
-VERSION=1.1
 
 os=$(uname -o|sed "s/\//-/")
 source="$(dirname "$(readlink -f "$0")")"
 target=$1
 
-# Read the settings
-INIFILE="${source}/${INIFILE}"
-if [[ ! -f "${INIFILE}" ]]; then
-   echo "[-] Could not find ${INIFILE}: required as it contains installer setings"
-   exit 1
-fi
-echo "Reading settings from ${INIFILE}"
-# shellcheck disable=SC1090
-source "${INIFILE}"
+
+read_settings() {
+    # Read the settings
+    INIFILE="${source}/${INIFILE}"
+    if [[ ! -f "${INIFILE}" ]]; then
+        echo "[-] Could not find ${INIFILE}: required as it contains installer setings"
+        exit 1
+    fi
+    echo "[*] Reading settings from ${INIFILE}"
+    # shellcheck disable=SC1090
+    source "${INIFILE}"
+}
 
 usage() {
     # Check whether the script is being executed from within the source directory
     if [ -z "$target" ]; then
         target=$(readlink -f .)
         if [ "${target}" == "${source}" ]; then
+            echo "installer.sh v${VERSION} - a generic installer script - PGCM - support@go-forward.net"
+            echo
             echo "Usage: installer.sh [TARGET]"
             echo "       or run from within target directory"
             exit
@@ -125,7 +143,19 @@ link_os_files() {
             fi
         done
     done
-    }
+}
+
+link_aliases() {
+    # Link files within the target directory
+    for targetfile in "${!aliases[@]}"; do
+        for linkname in ${aliases[$targetfile]}; do
+            if [[ -f "${target}/${targetfile}" ]]; then
+                ln -fv "${target}/${targetfile}" "${target}/${linkname}"
+            fi
+        done
+    done
+}
+
 
 check_executables() {
     # Check if executables are available
@@ -139,6 +169,8 @@ check_executables() {
     done
 }
 
+
+
 finish() {
     if [[ -n "$WARNING" ]]; then
         echo "[!] Done, but with warnings"
@@ -148,10 +180,12 @@ finish() {
     fi
 }
 
-echo "[*] Installing..."
 usage
+read_settings
+echo "[*] Installing..."
 copy_files
 link_files
 link_os_files
+link_aliases
 check_executables
 finish
